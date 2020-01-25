c66f6995bc2070d6f4eec643e8cad1559c9e3dcf
Wallace Mwabini <wallace@rawle.systems>
2020-01-25 12:55:49 +0300

    v1.1 - updated ip to 0.0.0.0

--- run_app.py
+++ run_app.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template, request
 from flaskext.mysql import MySQL
+#from flask_mysqldb import MySQL
 import sys
 import os
 
 myapp = Flask(__name__)
 
 myapp.config['MYSQL_HOST'] = 'localhost'
-myapp.config['MYSQL_USER'] = 'myflask'
+myapp.config['MYSQL_USER'] = 'root'
 myapp.config['MYSQL_DB'] = 'myflask'
-myapp.config['MYSQL_PASSWORD'] = 'flask'
+myapp.config['MYSQL_PASSWORD'] = ''
 
 mysql = MySQL(myapp)
+
 mysql.init_app(myapp)
 #cursor = mysql.get_db().cursor()
 
@@ -20,13 +22,17 @@ def georgerrmartin():
 	    firstName = request.form['fname']
 	    lastName = request.form['lname']
 	    cur = mysql.connection.cursor()
-	    cur.execute("INSERT INTO names(firstname, lastname) VALUES (%s, %s)", (firstName, lastName))
+	    cur.execute('''INSERT INTO names(firstname, lastname) VALUES (%s, %s)''', (firstName, lastName))
+	    
+	    #commit data to mysql
 	    mysql.connection.commit()
+
+	    #close mysql connection
 	    cur.close()
 	    return 'success'
 	return render_template('form.html')
 if __name__ == '__main__':
   myapp.debug = True
-  host = os.environ.get('IP','127.0.0.1')
+  host = os.environ.get('IP','0.0.0.0')
   port = int(os.environ.get('PORT','8081'))
   myapp.run(host=host, port=port)
